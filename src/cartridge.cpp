626992885cfee0110b388dffc0124d3cc7071827
Ho Tuan Kiet <tuankiet65@gmail.com>
2019-10-27 21:00:08 -0400

    Initial code for parsing cartridge files

---
+++ src/cartridge.cpp
@@ -0,0 +1,225 @@
+#include "cartridge.h"
+
+#include <fstream>
+#include <memory>
+#include <stdexcept>
+#include <string>
+
+#include "glog/logging.h"
+#include "fmt/format.h"
+#include "filesystem.h"
+
+namespace {
+
+static const size_t TITLE_BEGIN_ADDR = 0x0134;
+static const size_t TITLE_END_ADDR   = 0x0142;
+static const size_t GAMEBOY_TYPE_ADDR = 0x0143;
+static const size_t CARTRIDGE_TYPE_ADDR = 0x0147;
+static const size_t ROM_SIZE_ADDR = 0x0148;
+static const size_t RAM_SIZE_ADDR = 0x0149;
+static const size_t CARTRIDGE_TYPE_ADDR = 0x0147;
+static const size_t HEADER_BEGIN_ADDR = 0x0134;
+static const size_t HEADER_END_ADDR = 0x014c;
+static const size_t HEADER_CHECKSUM_ADDR = 0x014d;
+
+static const size_t KBYTE = 1024;
+
+std::string get_title(const CartridgeContent &content) {
+    std::string result;
+
+    for (size_t pos = TITLE_BEGIN_ADDR; pos <= TITLE_END_ADDR; ++pos) {
+        uint8_t byte = content.at(pos);
+        if (byte == 0)
+            break;
+        result.push_back(byte);
+    }
+
+    return result;
+}
+
+Cartridge::GameBoyType get_gameboy_type(const CartridgeContent &content) {
+    uint8_t raw_type = content.at(GAMEBOY_TYPE_ADDR);
+
+    if (raw_type == 0x80) {
+        return Cartridge::GameBoyType::ColorGameBoy;
+    }
+
+    return Cartridge::GameBoyType::GameBoy;
+}
+
+Cartridge::CartridgeType get_cartridge_type(const CartridgeContent &content) {
+    uint8_t raw_type = content.at(CARTRIDGE_TYPE_ADDR);
+
+    return static_cast<Cartridge::CartridgeType>(raw_type);
+}
+
+bool get_rom_size(const CartridgeContent &content, size_t *result) {
+    uint8_t raw_type = content.at(ROM_SIZE_ADDR);
+
+    switch (raw_type) {
+        case 0x00: *result =   32 * KBYTE; break;
+        case 0x01: *result =   64 * KBYTE; break;
+        case 0x02: *result =  128 * KBYTE; break;
+        case 0x03: *result =  256 * KBYTE; break;
+        case 0x04: *result =  512 * KBYTE; break;
+        case 0x05: *result = 1024 * KBYTE; break;
+        case 0x06: *result = 2048 * KBYTE; break;
+        case 0x52: *result = 1152 * KBYTE; break; // 1.1MByte
+        case 0x53: *result = 1280 * KBYTE; break; // 1.2MByte
+        case 0x54: *result = 1536 * KBYTE; break; // 1.5MByte
+
+        default:
+            LOG(ERROR) << fmt::format("Unknown ROM size {0:#x}", raw_type);
+            return false;
+    }
+
+    return true;
+}
+
+bool get_ram_size(const CartridgeContent &content, size_t *result) {
+    uint8_t raw_type = content.at(RAM_SIZE_ADDR);
+
+    switch (raw_type) {
+        case 0x00: *result = 0; break;
+        case 0x01: *result = 2 * KBYTE; break;
+        case 0x02: *result = 8 * KBYTE; break;
+        case 0x03: *result = 32 * KBYTE; break;
+        case 0x04: *result = 128 * KBYTE; break;
+
+        default:
+            LOG(ERROR) << fmt::format("Unknown RAM size {0:#x}", raw_type);
+            return false;
+    }
+
+    return true;
+}
+
+Cartridge::CartridgeType get_type(const CartridgeContent &content) {
+    uint8_t raw_type = content.at(CARTRIDGE_TYPE_ADDR);
+
+    return static_cast<Cartridge::CartridgeType>(raw_type);
+}
+
+uint8_t get_header_checksum(const CartridgeContent &content) {
+    return content.at(HEADER_CHECKSUM_ADDR);
+}
+
+uint8_t calculate_header_checksum(const CartridgeContent &content) {
+    uint8_t checksum = 0;
+
+    for (size_t i = HEADER_BEGIN_ADDR; i != HEADER_END_ADDR; ++i) {
+        checksum = checksum - content.at(i) - 1;
+    }
+
+    return checksum;
+}
+
+} // namespace
+
+
+Cartridge::Cartridge(
+    const std::string &title,
+    GameBoyType game_boy_type,
+    /* uint8_t licensee_code, */
+    /* SuperGameBoyIndicator sbg_indicator, */
+    CartridgeType type,
+    size_t rom_size,
+    size_t ram_size,
+    /* Region region, */
+    /* uint8_t mask_rom_version, */
+    const CartridgeContent &content)
+    : title_(title),
+      game_boy_type_(game_boy_type),
+    //   sbg_indicator_(sbg_indicator),
+      type_(type),
+      rom_size_(rom_size),
+      ram_size_(ram_size),
+    //   region_(region),
+    //   mask_rom_version_(mask_rom_version),
+      content_(content) {}
+
+// static
+bool Cartridge::from_file(fs::path path, std::unique_ptr<Cartridge> *cartridge) {
+    if (!fs::is_regular_file(path)) {
+        LOG(ERROR) << fmt::format("{} is not a regular file", path.string());
+        return false;
+    }
+
+    std::ifstream in(path.native(), std::ios::in | std::ios::binary);
+    if (!in) {
+        LOG(ERROR) << fmt::format("Unable to open file {}", path.string());
+        return false;
+    }
+
+    CartridgeContent content;
+    while (true) {
+        const size_t BUFFER_SIZE = 2048;
+        char buffer[BUFFER_SIZE];
+
+        in.read(buffer, BUFFER_SIZE);
+        const size_t read_count = in.gcount();
+
+        if (in.eof()) {
+            LOG(INFO) << "Reached the end of file";
+            break;
+        }
+
+        if (in.fail()) {
+            LOG(ERROR) << fmt::format("Error while reading file {}",
+                                      path.string());
+            return false;
+        }
+
+        // Append the entire buffer to the end of content
+        content.insert(content.end(), buffer, buffer + read_count);
+    }
+
+    // The information area is located between 0x0100 and 0x014f
+    if (content.size() < 0x0150) {
+        LOG(ERROR) <<
+            fmt::format(
+                "Cartridge size too small to contain the information area, "
+                "read size = {0:#x} bytes", content.size());
+        return false;
+    }
+
+    std::string title = get_title(content);
+    GameBoyType gameboy_type = get_gameboy_type(content);
+    CartridgeType type = get_type(content);
+
+    size_t rom_size;
+    if (!get_rom_size(content, &rom_size)) {
+        LOG(ERROR) << "Unable to get ROM size";
+        return false;
+    }
+
+    size_t ram_size;
+    if (!get_ram_size(content, &ram_size)) {
+        LOG(ERROR) << "Unable to get RAM size";
+        return false;
+    }
+
+    uint8_t header_checksum = get_header_checksum(content),
+            calculated_header_checksum = calculate_header_checksum(content);
+    if (calculated_header_checksum != header_checksum) {
+        LOG(WARNING) <<
+            fmt::format(
+                "Calculated header complement ({0:#x}) does not match "
+                "expected complement stored in header ({1:#x})",
+                calculate_header_checksum,
+                header_checksum);
+    }
+
+    // uint16_t checksum = get_checksum(content),
+    //          calculated_checksum = calculate_checksum(content);
+    // if (checksum != calculated_checksum) {
+    //     LOG(WARNING) << fmt::format(
+    //         "Calculated checksum ({0:#x}) does not match checksum in header "
+    //         "({1:#x})", calculated_checksum, checksum);
+    // }
+
+    *cartridge = std::make_unique<Cartridge>(
+        title, gameboy_type, type, rom_size, ram_size, content);
+
+    return true;
+}
--- src/knocknock.cpp
+++ src/knocknock.cpp
@@ -1,19 +1,17 @@
 #include <glog/logging.h>
 
-#include "cpu/register.h"
-#include "memory/mmu.h"
-#include "memory/ram.h"
+#include <memory>
+
+#include "cartridge.h"
 
 int main(int argc, char* argv[]) {
     google::InitGoogleLogging(argv[0]);
     google::InstallFailureSignalHandler();
 
-    cpu::Register reg;
-    reg.set_a(0xff);
-    reg.set_b(0xff);
+    std::unique_ptr<Cartridge> cartridge;
+    if (!Cartridge::from_file(argv[1], &cartridge)) {
+        LOG(ERROR) << "Unable to load cartridge";
+    }
 
-    memory::MMU mmu;
-
-    memory::RAM ram;
-    mmu.add_region(1, &ram, ram.START_ADDR, ram.END_ADDR);
+    return 0;
 }
