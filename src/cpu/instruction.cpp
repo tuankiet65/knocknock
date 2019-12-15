ce3b89e1653d1a313cc12059ce18caa4084bcbdf
Ho Tuan Kiet <tuankiet65@gmail.com>
2019-12-14 23:08:58 -0500

    cpu: Initial Instruction implementation
    The Instruction class represents a decoded opcode, in which the
    operation and operands are enums of possible operations and operands.
    An Instruction can be converted into the corresponding assembly.

--- src/cpu/instruction.cpp
+++ src/cpu/instruction.cpp
@@ -9,57 +9,8 @@
 
 namespace cpu {
 
-namespace {
-
-Instruction decode_cb(const memory::Memory *mem, memory::MemoryAddr *addr) {
-    uint8_t _opcode = mem->read(*addr);
-    (*addr)++;
-
-    uint8_t x = _opcode >> 6,             // first two bits
-        y = (_opcode & 0b00111000) >> 3,  // middle three bits
-        z = _opcode & 0b00000111;         // last three bits
-
-    Instruction::Operand lhs;
-    switch (z) {
-        case 0: lhs = Instruction::Operand::RegB; break;
-        case 1: lhs = Instruction::Operand::RegC; break;
-        case 2: lhs = Instruction::Operand::RegD; break;
-        case 3: lhs = Instruction::Operand::RegE; break;
-        case 4: lhs = Instruction::Operand::RegH; break;
-        case 5: lhs = Instruction::Operand::RegL; break;
-        case 6: lhs = Instruction::Operand::MemRegHL; break;
-        case 7: lhs = Instruction::Operand::RegA; break;
-        default: DCHECK(false) << fmt::format("Unknown z: {}", z); break;
-    }
-
-    Instruction::Opcode opcode;
-    switch (x) {
-        case 0:
-            switch (y) {
-                case 0: opcode = Instruction::Opcode::RLC; break;
-                case 1: opcode = Instruction::Opcode::RRC; break;
-                case 2: opcode = Instruction::Opcode::RL; break;
-                case 3: opcode = Instruction::Opcode::RR; break;
-                case 4: opcode = Instruction::Opcode::SLA; break;
-                case 5: opcode = Instruction::Opcode::SRA; break;
-                case 6: opcode = Instruction::Opcode::SLL; break;
-                case 7: opcode = Instruction::Opcode::SRL; break;
-                default:
-                    DCHECK(false) << fmt::format("Unknown y: {}", y);
-                    break;
-            }
-            break;
-        case 1: opcode = Instruction::Opcode::BIT; break;
-        case 2: opcode = Instruction::Opcode::RES; break;
-        case 3: opcode = Instruction::Opcode::SET; break;
-        default: DCHECK(false) << fmt::format("Unknown x: {}", x); break;
-    }
-
-    return Instruction(opcode, lhs, Instruction::Operand::None);
-}
-
-std::string disassemble_opcode(Instruction::Opcode opcode) {
-    switch (opcode) {
+std::string Instruction::disassemble_opcode() const {
+    switch (opcode_) {
         case Instruction::Opcode::RLC: return "RLC";
         case Instruction::Opcode::RRC: return "RRC";
         case Instruction::Opcode::RL: return "RL";
@@ -69,51 +20,141 @@ std::string disassemble_opcode(Instruction::Opcode opcode) {
         case Instruction::Opcode::SLL: return "SLL";
         case Instruction::Opcode::SRL: return "SRL";
         case Instruction::Opcode::BIT: return "BIT";
-        case Instruction::Opcode::RES: return "RES";
         case Instruction::Opcode::SET: return "SET";
-    }
+        case Instruction::Opcode::RES: return "RES";
+        case Instruction::Opcode::ADC: return "ADC";
+        case Instruction::Opcode::ADD: return "ADD";
+        case Instruction::Opcode::AND: return "AND";
+        case Instruction::Opcode::CALL: return "CALL";
+        case Instruction::Opcode::CCF: return "CCF";
+        case Instruction::Opcode::CP: return "CP";
+        case Instruction::Opcode::CPL: return "CPL";
+        case Instruction::Opcode::DAA: return "DAA";
+        case Instruction::Opcode::DEC: return "DEC";
+        case Instruction::Opcode::DI: return "DI";
+        case Instruction::Opcode::EI: return "EI";
+        case Instruction::Opcode::HALT: return "HALT";
+        case Instruction::Opcode::INC: return "INC";
+        case Instruction::Opcode::JP: return "JP";
+        case Instruction::Opcode::JR: return "JR";
+        case Instruction::Opcode::LD: return "LD";
+        case Instruction::Opcode::LDHL: return "LDHL";
+        case Instruction::Opcode::LDH: return "LDH";
+        case Instruction::Opcode::NOP: return "NOP";
+        case Instruction::Opcode::OR: return "OR";
+        case Instruction::Opcode::POP: return "POP";
+        case Instruction::Opcode::PUSH: return "PUSH";
+        case Instruction::Opcode::RET: return "RET";
+        case Instruction::Opcode::RETI: return "RETI";
+        case Instruction::Opcode::RLA: return "RLA";
+        case Instruction::Opcode::RLCA: return "RLCA";
+        case Instruction::Opcode::RRA: return "RRA";
+        case Instruction::Opcode::RRCA: return "RRCA";
+        case Instruction::Opcode::SBC: return "SBC";
+        case Instruction::Opcode::SCF: return "SCF";
+        case Instruction::Opcode::STOP: return "STOP";
+        case Instruction::Opcode::SUB: return "SUB";
+        case Instruction::Opcode::XOR: return "XOR";
 
-    DCHECK(false) << fmt::format("Unknown opcode: {}", opcode);
-    return fmt::format("[unknown opcode: {}]", opcode);
+        case Instruction::Opcode::RST_00H: return "RST 00H";
+        case Instruction::Opcode::RST_08H: return "RST 08H";
+        case Instruction::Opcode::RST_10H: return "RST 10H";
+        case Instruction::Opcode::RST_18H: return "RST 18H";
+        case Instruction::Opcode::RST_20H: return "RST 20H";
+        case Instruction::Opcode::RST_28H: return "RST 28H";
+        case Instruction::Opcode::RST_30H: return "RST 30H";
+        case Instruction::Opcode::RST_38H: return "RST 38H";
+    };
+
+    DCHECK(false) << fmt::format("Unknown opcode: {}", opcode_);
+    return fmt::format("[unknown opcode: {}]", opcode_);
 }
 
-std::string disassemble_operand(Instruction::Operand operand) {
+std::string Instruction::disassemble_operand(
+    Instruction::Operand operand) const {
     switch (operand) {
-        case Instruction::Operand::RegA: return "A";
-        case Instruction::Operand::RegB: return "B";
-        case Instruction::Operand::RegC: return "C";
-        case Instruction::Operand::RegD: return "D";
-        case Instruction::Operand::RegE: return "E";
-        case Instruction::Operand::RegH: return "H";
-        case Instruction::Operand::RegL: return "L";
-        case Instruction::Operand::MemRegHL: return "(HL)";
-    }
-}
+        case Instruction::Operand::PtrBC: return "(BC)";
+        case Instruction::Operand::PtrC: return "(C)";
+        case Instruction::Operand::PtrDE: return "(DE)";
+        case Instruction::Operand::PtrHL: return "(HL)";
+        case Instruction::Operand::PtrHLPlus: return "(HL+)";
+        case Instruction::Operand::PtrHLMinus: return "(HL-)";
+        case Instruction::Operand::A: return "A";
+        case Instruction::Operand::AF: return "AF";
+        case Instruction::Operand::B: return "B";
+        case Instruction::Operand::BC: return "BC";
+        case Instruction::Operand::C: return "C";
+        case Instruction::Operand::D: return "D";
+        case Instruction::Operand::DE: return "DE";
+        case Instruction::Operand::E: return "E";
+        case Instruction::Operand::H: return "H";
+        case Instruction::Operand::HL: return "HL";
+        case Instruction::Operand::L: return "L";
+        case Instruction::Operand::NC: return "NC";
+        case Instruction::Operand::NZ: return "NZ";
+        case Instruction::Operand::SP: return "SP";
+        case Instruction::Operand::Z: return "Z";
 
+        // Special handling for immediate operands
+        case Instruction::Operand::Imm8:
+            return fmt::format("{:#02X}", imm8_.value());
+        case Instruction::Operand::Imm8Sign:
+            return fmt::format("{:#02d}", imm8sign_.value());
+        case Instruction::Operand::PtrImm8:
+            return fmt::format("({:#02X})", imm8_.value());
+        case Instruction::Operand::Imm16:
+            return fmt::format("{:#04X}", imm16_.value());
+        case Instruction::Operand::PtrImm16:
+            return fmt::format("({:#04X})", imm16_.value());
+    }
+
+    DCHECK(false) << fmt::format("Unknown operand: {}", operand);
+    return fmt::format("[unknown operand: {}]", operand);
 }  // namespace
 
 Instruction::Instruction(Opcode opcode, Operand lhs, Operand rhs)
-    : opcode_(opcode), lhs_(lhs), rhs_(rhs) {}
+    : opcode_(opcode), lhs_(lhs), rhs_(rhs), imm8_(), imm8sign_(), imm16_() {}
 
-// static
+Instruction::Instruction(Opcode opcode, Operand lhs, Operand rhs, uint8_t imm8)
+    : opcode_(opcode),
+      lhs_(lhs),
+      rhs_(rhs),
+      imm8_(imm8),
+      imm8sign_(),
+      imm16_() {}
 
-std::optional<Instruction> Instruction::decode(const memory::Memory *mem,
-                                               memory::MemoryAddr *addr) {
-    uint8_t opcode = mem->read(*addr);
-    (*addr)++;
+Instruction::Instruction(Opcode opcode,
+                         Operand lhs,
+                         Operand rhs,
+                         int8_t imm8sign)
+    : opcode_(opcode),
+      lhs_(lhs),
+      rhs_(rhs),
+      imm8_(),
+      imm8sign_(imm8sign),
+      imm16_() {}
 
-    if (opcode == 0xcb)
-        return decode_cb(mem, addr);
-}
+Instruction::Instruction(Opcode opcode,
+                         Operand lhs,
+                         Operand rhs,
+                         uint16_t imm16)
+    : opcode_(opcode),
+      lhs_(lhs),
+      rhs_(rhs),
+      imm8_(),
+      imm8sign_(),
+      imm16_(imm16) {}
 
 std::string Instruction::disassemble() const {
-    std::string result = disassemble_opcode(opcode_);
+    std::string result = disassemble_opcode();
 
-    if (lhs_ != Instruction::Operand::None)
+    if (lhs_ != Instruction::Operand::None) {
         result += " " + disassemble_operand(lhs_);
 
-    if (rhs_ != Instruction::Operand::None)
-        result += ", " + disassemble_operand(rhs_);
+        if (rhs_ != Instruction::Operand::None) {
+            result += ", " + disassemble_operand(rhs_);
+        }
+    }
 
     return result;
 }
--- src/cpu/instruction.h
+++ src/cpu/instruction.h
@@ -9,23 +9,119 @@ namespace cpu {
 
 class Instruction {
 public:
-    enum class Opcode { RLC, RRC, RL, RR, SLA, SRA, SLL, SRL, BIT, RES, SET };
-    enum class Operand {
-        None,  // for when an opcode accepts 0 or 1 operands
-        RegA,
-        RegB,
-        RegC,
-        RegD,
-        RegE,
-        RegH,
-        RegL,
-        MemRegHL  // (HL)
+    enum class Opcode {
+        NOP,
+        RLC,
+        RRC,
+        RL,
+        RR,
+        SLA,
+        SRA,
+        SLL,
+        SRL,
+        BIT,
+        RES,
+        SET,
+        ADC,
+        ADD,
+        AND,
+        CALL,
+        CCF,
+        CP,
+        CPL,
+        DAA,
+        DEC,
+        DI,
+        EI,
+        HALT,
+        INC,
+        JP,
+        JR,
+        LD,
+        LDH,
+        LDHL,
+        OR,
+        POP,
+        PUSH,
+        RET,
+        RETI,
+        RLA,
+        RLCA,
+        RRA,
+        RRCA,
+        SBC,
+        SCF,
+        STOP,
+        SUB,
+        XOR,
+
+        // Resets
+        RST_00H,
+        RST_08H,
+        RST_10H,
+        RST_18H,
+        RST_20H,
+        RST_28H,
+        RST_30H,
+        RST_38H,
     };
 
+    enum class Operand {
+        None,  // for when an opcode accepts 0 or 1 operands
+
+        // 8 bit registers
+        A,
+        B,
+        C,
+        PtrC,  // (C), (FF00 + C)
+        D,
+        E,
+        H,
+        L,
+
+        // 16 bit registers and its pointers
+        AF,
+        BC,
+        PtrBC,  // (BC)
+        DE,
+        PtrDE,  // (DE)
+        HL,
+        PtrHL,       // (HL)
+        PtrHLPlus,   // (HL+)
+        PtrHLMinus,  // (HL-)
+        SP,
+
+        // Individual flag in the flag register
+        NC,  // No carry?
+        NZ,  // Not zero
+        Z,   // Zero
+
+        // Immediates
+        Imm8,      // uint8_t
+        PtrImm8,   // (Imm8) = (FF00 + Imm8)
+        Imm8Sign,  // int8_t
+        Imm16,     // uint16_t
+        PtrImm16,  // (Imm16)
+    };
+
+    // Instruction(Opcode opcode);
+    // Instruction(Opcode opcode, Operand lhs);
     Instruction(Opcode opcode, Operand lhs, Operand rhs);
 
-    static std::optional<Instruction> decode(const memory::Memory *mem,
-                                             memory::MemoryAddr *addr);
+    // Mark these constructors as explicit to prevent type conversion between
+    // uint8_t, int8_t and uint16_t.
+    explicit Instruction(Opcode opcode, Operand lhs, Operand rhs, uint8_t imm8);
+    explicit Instruction(Opcode opcode,
+                         Operand lhs,
+                         Operand rhs,
+                         int8_t imm8sign);
+    explicit Instruction(Opcode opcode,
+                         Operand lhs,
+                         Operand rhs,
+                         uint16_t imm16);
+
+    // static std::optional<Instruction> decode(const memory::Memory *mem,
+    //                                          memory::MemoryAddr *addr);
 
     Opcode opcode() const { return opcode_; }
     Operand lhs() const { return lhs_; }
@@ -34,8 +130,15 @@ public:
     std::string disassemble() const;
 
 private:
+    std::string disassemble_opcode() const;
+    std::string disassemble_operand(Operand operand) const;
+
     Opcode opcode_;
     Operand lhs_, rhs_;
+
+    std::optional<uint8_t> imm8_;
+    std::optional<int8_t> imm8sign_;
+    std::optional<uint16_t> imm16_;
 };
 
 }  // namespace cpu
