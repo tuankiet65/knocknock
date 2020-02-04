1d31a9b48292175622a9e2b10d57c4e98bf2f857
Ho Tuan Kiet <tuankiet65@gmail.com>
2020-02-04 04:02:09 -0500

    cpu/operands: update unittest to reflect changes to remove operand name

--- src/cpu/operands_unittest.cpp
+++ src/cpu/operands_unittest.cpp
@@ -6,18 +6,14 @@
 #include "memory/test_memory.h"
 
 TEST_CASE("Register8", "[cpu][operands]") {
-    cpu::Register8 reg8("A");
-
-    REQUIRE(reg8.name() == "A");
+    cpu::Register8 reg8;
 
     reg8.write(0x69);
     REQUIRE(reg8.read() == 0x69);
 }
 
 TEST_CASE("FlagRegister", "[cpu][operands]") {
-    cpu::FlagRegister f("F");
-
-    REQUIRE(f.name() == "F");
+    cpu::FlagRegister f;
 
     // All off
     f.write(0b00000000);
@@ -57,21 +53,15 @@ TEST_CASE("FlagRegister", "[cpu][operands]") {
 }
 
 TEST_CASE("Register16", "[cpu][operands]") {
-    cpu::Register16 reg16("BC");
-
-    REQUIRE(reg16.name() == "BC");
+    cpu::Register16 reg16;
 
     reg16.write(0x1234);
     REQUIRE(reg16.read() == 0x1234);
 }
 
 TEST_CASE("Register16Mirror", "[cpu][operands]") {
-    cpu::Register8 msb("B"), lsb("C");
+    cpu::Register8 msb, lsb;
     cpu::Register16Mirror reg16(&msb, &lsb);
-
-    REQUIRE(msb.name() == "B");
-    REQUIRE(lsb.name() == "C");
-    REQUIRE(reg16.name() == "BC");
 
     msb.write(0x12);
     lsb.write(0x34);
@@ -88,14 +78,12 @@ TEST_CASE("Register16Mirror", "[cpu][operands]") {
 TEST_CASE("Immediate8", "[cpu][operands]") {
     cpu::Immediate8 imm8(69);
 
-    REQUIRE(imm8.name() == "69");
     REQUIRE(imm8.read() == 69);
 }
 
 TEST_CASE("Immediate16", "[cpu][operands]") {
     cpu::Immediate16 imm16(9988);
 
-    REQUIRE(imm16.name() == "9988");
     REQUIRE(imm16.read() == 9988);
 }
 
@@ -108,25 +96,21 @@ TEST_CASE("Memory8", "[cpu][operands]") {
     SECTION("Memory8 + Immediate16") {
         cpu::Immediate16 addr(1234);
 
-        REQUIRE(addr.name() == "1234");
         REQUIRE(addr.read() == 1234);
 
         cpu::Memory8 mem8(&mem, addr);
 
-        REQUIRE(mem8.name() == "(1234)");
         REQUIRE(mem8.read() == 0x69);
     }
 
     SECTION("Memory8 + Register16") {
-        cpu::Register16 addr("NN");
+        cpu::Register16 addr;
         addr.write(1234);
 
-        REQUIRE(addr.name() == "NN");
         REQUIRE(addr.read() == 1234);
 
         cpu::Memory8 mem8(&mem, addr);
 
-        REQUIRE(mem8.name() == "(NN)");
         REQUIRE(mem8.read() == 0x69);
     }
 }
@@ -140,25 +124,21 @@ TEST_CASE("Memory16", "[cpu][operands]") {
     SECTION("Memory16 + Immediate16") {
         cpu::Immediate16 addr(1234);
 
-        REQUIRE(addr.name() == "1234");
         REQUIRE(addr.read() == 1234);
 
         cpu::Memory16 mem16(&mem, addr);
 
-        REQUIRE(mem16.name() == "(1234)");
         REQUIRE(mem16.read() == 0x6942);
     }
 
     SECTION("Memory16 + Register16") {
-        cpu::Register16 addr("NN");
+        cpu::Register16 addr;
         addr.write(1234);
 
-        REQUIRE(addr.name() == "NN");
         REQUIRE(addr.read() == 1234);
 
         cpu::Memory16 mem16(&mem, addr);
 
-        REQUIRE(mem16.name() == "(NN)");
         REQUIRE(mem16.read() == 0x6942);
     }
 }
