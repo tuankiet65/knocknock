8fa3138ac36878046e792dd59d5f0f66f524cceb
Ho Tuan Kiet <tuankiet65@gmail.com>
2020-02-09 00:39:36 -0500

    memory/test_memory: add docs

--- src/memory/test_memory.h
+++ src/memory/test_memory.h
@@ -4,9 +4,11 @@
 
 namespace memory {
 
-// A memory that allows read and write access everywhere in its entire 16-bit
-// memory space (0x0000 - 0xffff). Shall only be used in unittests so one
-// wouldn't need to care about the valid address space.
+/**
+ * A memory that allows read and write access everywhere in its entire 16-bit
+ * memory space (0x0000 - 0xffff). Shall only be used in unittests so one
+ * wouldn't need to care about the valid address space.
+ */
 class TestMemory : public Memory {
 public:
     TestMemory();
