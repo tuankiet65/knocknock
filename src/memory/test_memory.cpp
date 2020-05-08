6ab4ab0eba89018bba4d18df567bec10ed251e63
Ho Tuan Kiet <tuankiet65@gmail.com>
2020-05-08 16:56:57 -0400

    memory/TestMemory: make it ROMLoadableMemory

--- src/memory/test_memory.cpp
+++ src/memory/test_memory.cpp
@@ -12,4 +12,13 @@ void TestMemory::write(MemoryAddr addr, MemoryValue value) {
     memory_[addr] = value;
 }
 
+bool TestMemory::load_rom(const std::vector<MemoryValue> &rom) {
+    if (rom.size() > sizeof(memory_)) {
+        return false;
+    }
+
+    std::copy(rom.begin(), rom.end(), memory_);
+    return true;
+}
+
 }  // namespace memory
--- src/memory/test_memory.h
+++ src/memory/test_memory.h
@@ -9,12 +9,13 @@ namespace memory {
  * memory space (0x0000 - 0xffff). Shall only be used in unittests so one
  * wouldn't need to care about the valid address space.
  */
-class TestMemory : public Memory {
+class TestMemory : public ROMLoadableMemory {
 public:
     TestMemory();
 
     MemoryValue read(MemoryAddr addr) const override;
     void write(MemoryAddr addr, MemoryValue value) override;
+    bool load_rom(const std::vector<MemoryValue> &rom) override;
 
 private:
     MemoryValue memory_[0x10000];
