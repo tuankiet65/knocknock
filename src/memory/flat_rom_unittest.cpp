ddc59433619897c75b4ba1209ebb925d83d1e947
Ho Tuan Kiet <tuankiet65@gmail.com>
2020-05-10 00:19:33 -0400

    memory/FlatROM: move test cases inside namespace memory

--- src/memory/flat_rom_unittest.cpp
+++ src/memory/flat_rom_unittest.cpp
@@ -4,46 +4,48 @@
 
 #include "memory/regions.h"
 
+namespace memory {
+
 TEST_CASE("ROM read + write", "[memory][FlatROM]") {
-    memory::FlatROM flat_rom(0);
+    FlatROM flat_rom(0);
 
     SECTION("Bank 0") {
-        for (memory::MemoryAddr i = memory::ROM_0_BEGIN; i <= memory::ROM_0_END;
-             ++i) {
+        for (MemoryAddr i = ROM_0_BEGIN; i <= ROM_0_END; ++i) {
             flat_rom.write(i, i % 256);
         }
 
-        for (memory::MemoryAddr i = memory::ROM_0_BEGIN; i <= memory::ROM_0_END;
-             ++i) {
+        for (MemoryAddr i = ROM_0_BEGIN; i <= ROM_0_END; ++i) {
             REQUIRE(flat_rom.read(i) == (i % 256));
         }
     }
 
     SECTION("Switchable bank") {
-        for (memory::MemoryAddr i = memory::ROM_SWITCHABLE_BEGIN;
-             i <= memory::ROM_SWITCHABLE_END; ++i) {
+        for (MemoryAddr i = ROM_SWITCHABLE_BEGIN; i <= ROM_SWITCHABLE_END;
+             ++i) {
             flat_rom.write(i, i % 128);
         }
 
-        for (memory::MemoryAddr i = memory::ROM_SWITCHABLE_BEGIN;
-             i <= memory::ROM_SWITCHABLE_END; ++i) {
+        for (MemoryAddr i = ROM_SWITCHABLE_BEGIN; i <= ROM_SWITCHABLE_END;
+             ++i) {
             REQUIRE(flat_rom.read(i) == (i % 128));
         }
     }
 }
 
 TEST_CASE("RAM read + write", "[memory][FlatROM]") {
-    const memory::MemorySize size = 0x100;
+    const MemorySize size = 0x100;
 
-    memory::FlatROM flat_rom(size);
+    FlatROM flat_rom(size);
 
-    for (memory::MemoryAddr i = memory::RAM_EXTERNAL_BEGIN;
-         i < memory::RAM_EXTERNAL_BEGIN + size; ++i) {
+    for (MemoryAddr i = RAM_EXTERNAL_BEGIN; i < RAM_EXTERNAL_BEGIN + size;
+         ++i) {
         flat_rom.write(i, i % 256);
     }
 
-    for (memory::MemoryAddr i = memory::RAM_EXTERNAL_BEGIN;
-         i < memory::RAM_EXTERNAL_BEGIN + size; ++i) {
+    for (MemoryAddr i = RAM_EXTERNAL_BEGIN; i < RAM_EXTERNAL_BEGIN + size;
+         ++i) {
         REQUIRE(flat_rom.read(i) == (i % 256));
     }
 }
+
+}  // namespace memory
