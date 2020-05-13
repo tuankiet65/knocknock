3d32e1e5520964abb75a92024d7a253eeb6c4926
Ho Tuan Kiet <tuankiet65@gmail.com>
2020-05-13 04:10:43 -0400

    cpu/cpu: do not initialize I/O registers

--- src/cpu/cpu.cpp
+++ src/cpu/cpu.cpp
@@ -59,39 +59,6 @@ CPU::CPU(memory::Memory *memory)
     hl_.write(0x014d);
     sp_.write(0xfffe);
     pc_ = 0x0100;
-
-    // initialize IO register
-    mem_->write(0xff05, 0x00);  //  TIMA
-    mem_->write(0xff06, 0x00);  //  TMA
-    mem_->write(0xff07, 0x00);  //  TAC
-    mem_->write(0xff10, 0x80);  //  NR10
-    mem_->write(0xff11, 0xbf);  //  NR11
-    mem_->write(0xff12, 0xf3);  //  NR12
-    mem_->write(0xff14, 0xbf);  //  NR14
-    mem_->write(0xff16, 0x3f);  //  NR21
-    mem_->write(0xff17, 0x00);  //  NR22
-    mem_->write(0xff19, 0xbf);  //  NR24
-    mem_->write(0xff1a, 0x7f);  //  NR30
-    mem_->write(0xff1b, 0xff);  //  NR31
-    mem_->write(0xff1c, 0x9f);  //  NR32
-    mem_->write(0xff1e, 0xbf);  //  NR33
-    mem_->write(0xff20, 0xff);  //  NR41
-    mem_->write(0xff21, 0x00);  //  NR42
-    mem_->write(0xff22, 0x00);  //  NR43
-    mem_->write(0xff23, 0xbf);  //  NR30
-    mem_->write(0xff24, 0x77);  //  NR50
-    mem_->write(0xff25, 0xf3);  //  NR51
-    mem_->write(0xff26, 0xf1);  //  NR52
-    mem_->write(0xff40, 0x91);  //  LCDC
-    mem_->write(0xff42, 0x00);  //  SCY
-    mem_->write(0xff43, 0x00);  //  SCX
-    mem_->write(0xff45, 0x00);  //  LYC
-    mem_->write(0xff47, 0xfc);  //  BGP
-    mem_->write(0xff48, 0xff);  //  OBP0
-    mem_->write(0xff49, 0xff);  //  OBP1
-    mem_->write(0xff4a, 0x00);  //  WY
-    mem_->write(0xff4b, 0x00);  //  WX
-    mem_->write(0xffff, 0x00);  //  IE
 }
 
 std::optional<Operand8 *> CPU::get_operand8(Operand operand) {
