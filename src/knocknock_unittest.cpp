9c56bc09df3ed77015adab74460cca55bd992efb
Ho Tuan Kiet <tuankiet65@gmail.com>
2020-05-15 00:08:11 -0400

    unittest: initialize glog before running the test suite

--- src/knocknock_unittest.cpp
+++ src/knocknock_unittest.cpp
@@ -1,2 +1,21 @@
-#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_RUNNER
+
+#include <glog/logging.h>
 #include <catch2/catch.hpp>
+
+int main(int argc, char *argv[]) {
+    google::InitGoogleLogging(argv[0]);
+    google::InstallFailureSignalHandler();
+
+    // Write log to stderr
+    FLAGS_logtostderr = true;
+
+    // Enable log color.
+    FLAGS_colorlogtostderr = true;
+
+    // Log messages at level WARNING and above by default.
+    FLAGS_minloglevel = google::GLOG_WARNING;
+
+    // Then run the test suite.
+    return Catch::Session().run(argc, argv);
+}
